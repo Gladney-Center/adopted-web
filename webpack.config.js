8d3312fc48fea96f1e8248853b1c13780176c042
David Paul <enlightenedpie@users.noreply.github.com>
2019-01-29 15:36:28 -0600

    attempting webpack config; added backup stylesheet

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const globImporter = require('node-sass-glob-importer')
+
+module.exports = (env) => {
+
+    return {
+        entry: ['@babel/polyfill','./web/app/themes/adopted/assets/js/entry.js'],
+        output: {
+            path: path.resolve(__dirname, 'web'),
+            filename: './web/app/themes/adopted/adopted.min.js',
+            publicPath: '/'
+        },
+        devServer: {
+            publicPath: '/',
+            port: 8888
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.sass$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'resolve-url-loader'
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                                importer: globImporter()
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(js)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.(eot|svg|ttf|woff|woff2|gif|png|jpg)$/,
+                    exclude: /node_modules/,
+                    loader: 'file-loader'
+                  }
+            ]
+        },
+        performance: {
+            hints: false
+        },
+        devServer: {
+            historyApiFallback: true,
+        },
+        plugins: [
+            new ExtractTextPlugin({
+                filename: './web/app/themes/adopted/style.css',
+                disable: false,
+                allChunks: true
+            }),
+            /* new HtmlWebpackPlugin({
+                filename: 'app.html',
+                template: 'templates/app.html',
+                title: 'SupertutorTV Courses',
+                inject : true,
+                hash: true
+            }), */
+            new webpack.DefinePlugin({ 'process.env.APP_MODE': JSON.stringify(env.APP_MODE) })
+        ]
+    }
+}
