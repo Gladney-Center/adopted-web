aeba3d7740e17ce6ac93603ef2c76c25d8b06b0c
David Paul <david.crouch@gladney.org>
2019-02-12 16:21:54 -0600

    DEV - updating body classes

--- web/app/themes/adopted/front-page.php
+++ web/app/themes/adopted/front-page.php
@@ -2,6 +2,13 @@
 
 /* Template Name: Front Page */
 
+add_filter( 'body_class', function($classes) {
+	$newcls = ['adpt-front-page'];
+	if (isset($classes['logged-in'])) $newcls[] = 'logged-in';
+	if (isset($classes['admin-bar'])) $newcls[] = 'admin-bar';
+	return $newcls;
+});
+
 global $post; 
 get_header();
 ?>
