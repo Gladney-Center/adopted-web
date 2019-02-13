b2f78ecad2cad118718062ee8590c0b48be614ad
David Paul <david.crouch@gladney.org>
2019-02-13 16:56:43 -0600

    DEV - style changes to footer block; wrapped with inner block for smaller EM size

--- web/app/themes/adopted/footer.php
+++ web/app/themes/adopted/footer.php
@@ -1,18 +1,20 @@
 <footer class="adpt-footer">
 	<div class="adpt-footer-block">
-		<div class="adpt-footer-heading">
-			<img class="constrained" src="<?php header_image(); ?>" alt="<?php echo bloginfo('name'); ?>" />
-		</div>
-		<div class="adpt-footer-info">
-			<span class="footer-info-inner">
-				<?php echo get_theme_mod('footer_block_1_email'); ?>
-			</span>
-			<span class="footer-info-inner">
-				<?php echo get_theme_mod('footer_block_1_phone'); ?>
-			</span>
-			<span class="footer-info-inner">
-				<?php echo get_theme_mod('footer_block_1_address'); ?>
-			</span>
+		<div class="adpt-footer-block-inner">
+			<div class="adpt-footer-heading">
+				<img class="constrained" src="<?php header_image(); ?>" alt="<?php echo bloginfo('name'); ?>" />
+			</div>
+			<div class="adpt-footer-info">
+				<span class="footer-info-inner">
+					<?php echo get_theme_mod('footer_block_1_email'); ?>
+				</span>
+				<span class="footer-info-inner">
+					<?php echo get_theme_mod('footer_block_1_phone'); ?>
+				</span>
+				<span class="footer-info-inner">
+					<?php echo get_theme_mod('footer_block_1_address'); ?>
+				</span>
+			</div>
 		</div>
 	</div>
 	<div class="adpt-footer-block"></div>
