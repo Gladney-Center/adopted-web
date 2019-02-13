2eba1397d437fda0a5861a67742eafa09bb615cd
David Paul <david.crouch@gladney.org>
2019-02-13 16:40:19 -0600

    DEV - testing customizer text in footer

--- web/app/themes/adopted/footer.php
+++ web/app/themes/adopted/footer.php
@@ -1,10 +1,13 @@
 <footer class="adpt-footer">
 	<div class="adpt-footer-block">
-		<img
-			class="constrained" 
-			src="<?php header_image(); ?>"
-			alt="<?php echo bloginfo('name'); ?>"
-		/>
+		<div class="adpt-footer-heading">
+			<img class="constrained" src="<?php header_image(); ?>" alt="<?php echo bloginfo('name'); ?>" />
+		</div>
+		<div class="adpt-footer-info">
+			<span class="bb-inner">
+				<?php echo get_theme_mod('footer_block_1_email'); ?>
+			</span>
+		</div>
 	</div>
 	<div class="adpt-footer-block"></div>
 	<div class="adpt-footer-block"></div>
