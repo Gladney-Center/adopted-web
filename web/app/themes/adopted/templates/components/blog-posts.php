078a4639d238290a658a6804c5057df7ab09099c
David Paul <david.crouch@gladney.org>
2019-02-15 13:40:03 -0600

    AWA-10 #comment building blog block styles and layout before dynamic loading with js

--- web/app/themes/adopted/templates/components/blog-posts.php
+++ web/app/themes/adopted/templates/components/blog-posts.php
@@ -8,15 +8,27 @@
 		</div>
 		<div class="adpt-post-blocks">
 			<div class="adpt-post-block">
-				<div class="adpt-post-block-inner"></div>
+				<div class="adpt-post-block-inner">
+					<div class="view-post-link">
+						<a href="#">View Post <i class="fas fa-long-arrow-alt-right"></i></a>
+					</div>
+				</div>
 			</div>
 			<div class="adpt-post-spacer">M</div>
 			<div class="adpt-post-block">
-				<div class="adpt-post-block-inner"></div>
+				<div class="adpt-post-block-inner">
+					<div class="view-post-link">
+						<a href="#">View Post <i class="fas fa-long-arrow-alt-right"></i></a>
+					</div>
+				</div>
 			</div>
 			<div class="adpt-post-spacer">M</div>
 			<div class="adpt-post-block">
-				<div class="adpt-post-block-inner"></div>
+				<div class="adpt-post-block-inner">
+					<div class="view-post-link">
+						<a href="#">View Post <i class="fas fa-long-arrow-alt-right"></i></a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
