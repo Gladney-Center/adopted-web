1877efbf2a253ba4ef3059429ca04ecc3fb98b8a
David Paul <david.crouch@gladney.org>
2019-02-15 12:25:33 -0600

    AWA-10 same as last commit

--- web/app/themes/adopted/templates/components/blog-posts.php
+++ web/app/themes/adopted/templates/components/blog-posts.php
@@ -2,7 +2,9 @@
 	<div class="adpt-blog-inner">
 		<div class="adpt-blog-top">
 			<div class="adpt-blog-heading"><h3>Blog</h3></div>
-			<div class="adpt-blog-link"></div>
+			<div class="adpt-blog-link">
+				<a class="btn" href="#">View More</a>
+			</div>
 		</div>
 	</div>
 </section>
