5357498e9309b4ba294bdecdecbdbdd1319663f2
David Paul <david.crouch@gladney.org>
2019-02-15 10:33:22 -0600

    DEV - finalizing styles on block 4 / gladney logo

--- web/app/themes/adopted/footer.php
+++ web/app/themes/adopted/footer.php
@@ -52,7 +52,12 @@
 			<div class="adpt-footer-heading">
 				<h4><?php echo unbold_filter(get_theme_mod('footer_block_4_heading')); ?></h4>
 			</div>
-			<?php echo wp_get_attachment_image(get_theme_mod('footer_block_4_logo'),'full'); ?>
+			<?php echo wp_get_attachment_image(
+				get_theme_mod('footer_block_4_logo'),
+				'full',
+				'',
+				['class' => 'constrained']
+			); ?>
 		</div>
 	</div>
 </footer>
