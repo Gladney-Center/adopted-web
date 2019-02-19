6aa9739ec1a89a24635b0300637c9204ed59b661
David Paul <david.crouch@gladney.org>
2019-02-19 14:41:28 -0600

    DEV - changed last type of element from section to div

--- web/app/themes/adopted/templates/components/blog-posts.php
+++ web/app/themes/adopted/templates/components/blog-posts.php
@@ -1,4 +1,4 @@
-<section class="adpt-blog">
+<div class="adpt-blog">
 	<div class="adpt-blog-inner">
 		<div class="adpt-blog-top">
 			<div class="adpt-blog-heading"><h3>Blog</h3></div>
@@ -32,4 +32,4 @@
 			</div>
 		</div>
 	</div>
-</section>
+</div>
