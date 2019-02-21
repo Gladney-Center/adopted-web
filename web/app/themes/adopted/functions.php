415dd3882d10b8cdd2ec61d3bb2051f8ba4bc551
David Paul <david.crouch@gladney.org>
2019-02-21 09:27:33 -0600

    DEV - yuh yuh

--- web/app/themes/adopted/functions.php
+++ web/app/themes/adopted/functions.php
@@ -12,4 +12,10 @@ function fa_filter($text) {
 	return preg_replace('(<fa:(.*?)>)', '<i class="fas fa-$1"></i>', $text);
 }
 
+add_filter('render_block_data',function($block,$source_block) {
+	print_r($block);
+	echo '<br>';
+	print_r($source_block);
+}, 11, 2);
+
 require_once __DIR__ . '/lib/customizer.php';
