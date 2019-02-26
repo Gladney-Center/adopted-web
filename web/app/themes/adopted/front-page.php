287e30c67795d7dda14de07f289ba08e2c966c21
David Paul <david.crouch@gladney.org>
2019-02-26 11:45:55 -0600

    DEV - testing customizer instead of block

--- web/app/themes/adopted/front-page.php
+++ web/app/themes/adopted/front-page.php
@@ -10,7 +10,16 @@ add_filter( 'body_class', function($classes) use ($meta) {
 });
  
 get_header(); ?>
-<pre>
-<?php print_r($meta); ?>
-</pre>
+<section class="adpt-stage">
+	<div class="wp-block-columns adpt-stage-inner">
+		<div class="wp-block-column">
+			<h1>only 17% of students believe adoption is a realistic option for them if they were to face an unplanned pregnancy.</h1>
+		</div>
+		<div class="wp-block-column">
+			<figure class="wp-block-image">
+				<img src="https://dev.adoption-education.com/app/uploads/2019/02/gabby-pregnant-thtbub.png" alt="Gabby is pregnant" class="wp-image-936">
+			</figure>
+		</div>
+	</div>
+</section>
 <?php get_footer();
