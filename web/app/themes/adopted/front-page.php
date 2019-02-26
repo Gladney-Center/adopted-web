0b842dc1e76a4b33b9743fb0956467d7f610cb57
David Paul <david.crouch@gladney.org>
2019-02-26 15:52:02 -0600

    DEV - testing

--- web/app/themes/adopted/front-page.php
+++ web/app/themes/adopted/front-page.php
@@ -30,4 +30,34 @@ get_header(); ?>
 		</div>
 	</div>
 </section>
+<section class="adpt-panel-block">
+	<div class="adpt-panel-block-inner">
+		<div class="adpt-panel">
+			<figure class="wp-block-image"><img src="https://dev.adoption-education.com/app/uploads/2019/02/Students-opt1.png" alt="AdoptED Students section background photo" class="wp-image-974"></figure>
+			<div class="adpt-panel-overlay">
+				<div class="adpt-panel-topright">
+					<?php Filter::svg('<adpt-icon:right-arrow>'); ?>
+					<span class="adpt-panel-vert">Learn More</span>
+				</div>
+				<div class="adpt-panel-info">
+					<h4>students</h4>
+					<p>Learn what adoption really means in an engaging class that you definitely won't sleep through!</p>
+				</div>
+			</div>
+		</div>
+		<div class="adpt-panel">
+			<figure class="wp-block-image"><img src="https://dev.adoption-education.com/app/uploads/2019/02/educators-opt1.png" alt="AdoptED Educators panel background image" class="wp-image-975"></figure>
+			<div class="adpt-panel-overlay">
+				<div class="adpt-panel-topright">
+					<?php Filter::svg('<adpt-icon:right-arrow>'); ?>
+					<span class="adpt-panel-vert">Learn More</span>
+				</div>
+				<div class="adpt-panel-info">
+					<h4>educators</h4>
+					<p>Learn more about free adoption education curriculum that engages your students and teaches them critical thinking skills and so much more!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <?php get_footer();
