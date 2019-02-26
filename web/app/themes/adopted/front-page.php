501005004e1459b65ef852d821fedb3d760286b6
David Paul <david.crouch@gladney.org>
2019-02-26 16:36:41 -0600

    DEV - fixed SVG hiccup

--- web/app/themes/adopted/front-page.php
+++ web/app/themes/adopted/front-page.php
@@ -37,7 +37,7 @@ get_header(); ?>
 				<figure class="wp-block-image"><img src="https://dev.adoption-education.com/app/uploads/2019/02/Students-opt1.png" alt="AdoptED Students section background photo" class="wp-image-974"></figure>
 				<div class="adpt-panel-overlay">
 					<div class="adpt-panel-topright">
-						<?php echo Filter::svg('<adpt-icon:right-arrow>'); ?>
+						<?php echo Filter::svg(' <adpt-icon:rightarrow> '); ?>
 						<span class="adpt-panel-vert">Learn More</span>
 					</div>
 					<div class="adpt-panel-info">
@@ -52,7 +52,7 @@ get_header(); ?>
 				<figure class="wp-block-image"><img src="https://dev.adoption-education.com/app/uploads/2019/02/educators-opt1.png" alt="AdoptED Educators panel background image" class="wp-image-975"></figure>
 				<div class="adpt-panel-overlay">
 					<div class="adpt-panel-topright">
-						<?php echo Filter::svg('<adpt-icon:right-arrow>'); ?>
+						<?php echo Filter::svg(' <adpt-icon:rightarrow> '); ?>
 						<span class="adpt-panel-vert">Learn More</span>
 					</div>
 					<div class="adpt-panel-info">
