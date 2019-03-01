a035ed74d0b7ab0c6f465a414e123e29c4d722ce
David Paul <david.crouch@gladney.org>
2019-03-01 16:29:08 -0600

    DEv - trying different symbols for sms block

--- web/app/themes/adopted/front-page.php
+++ web/app/themes/adopted/front-page.php
@@ -124,10 +124,10 @@ get_header(); ?>
 <section class="adpt-sms-facts">
 	<div class="adpt-sms-inner">
 		<div class="adpt-sms-aqua-box">
-			<div class="adpt-sms-arrow teal-bg"><?php echo Filter::fa('<fa:angle-right>'); ?></div>
+			<div class="adpt-sms-arrow teal-bg">&rsaquo;<?php //echo Filter::fa('<fa:angle-right>'); ?></div>
 			<div class="adpt-sms-text">
 				<span class="sms">18% of women at risk</span>
-				<span class="send">18% of women at risk</span>
+				<span class="send">&uarr;</span>
 			</div>
 		</div>
 	</div>
