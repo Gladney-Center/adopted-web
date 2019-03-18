380e025482020db40493b325f884ad643d26bb21
David Paul <david.crouch@gladney.org>
2019-03-18 15:15:03 -0500

    AWA-12 #comment added post type for front page statistics #time 1h

--- web/app/themes/adopted/functions.php
+++ web/app/themes/adopted/functions.php
@@ -8,5 +8,7 @@ date_default_timezone_set('America/Chicago');
 
 require_once __DIR__ . '/lib/filter.class.php';
 require_once __DIR__ . '/lib/customizer.php';
+require_once __DIR__ . '/lib/post_types.class.php';
 
 Customizer::init();
+Post_Types::init();
