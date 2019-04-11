65539075b329a78e18b16fd4742c5ab9d3c34366
David Paul <david.crouch@gladney.org>
2019-04-11 15:36:20 -0500

    AWA-12 #comment same as last commit #time 1m

--- web/app/themes/adopted/templates/components/blog-posts.php
+++ web/app/themes/adopted/templates/components/blog-posts.php
@@ -9,14 +9,13 @@
 		<div class="adpt-post-blocks">
 		<?php
 			$HAPIkey = HS_HAPI_KEY;
-			$blogcache = false; //get_transient('adpt_blog_cache');
+			$blogcache = get_transient('adpt_blog_cache');
 
 			if ($blogcache === false) {
 				$blogcache = wp_remote_get("https://api.hubapi.com/content/api/v2/blog-posts?hapikey=$HAPIkey&limit=3&state=PUBLISHED");
 				set_transient('adpt_blog_cache',$blogcache,WEEK_IN_SECONDS);
 			}
 			$blog_body = json_decode($blogcache['body'], true);
-			print_r($blog_body);
 
 			foreach ($blog_body['objects'] as $obj) : ?>
 				<div class="adpt-post-block">
