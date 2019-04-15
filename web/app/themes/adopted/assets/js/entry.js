f49b4e039b4004839dd91eac99b439e09670cdad
David Paul <david.crouch@gladney.org>
2019-04-15 16:57:22 -0500

    DEV - gckhfc

--- web/app/themes/adopted/assets/js/entry.js
+++ web/app/themes/adopted/assets/js/entry.js
@@ -4,5 +4,9 @@
 		isInViewport = (elem) => elem.getBoundingClientRect().top < 800
 
 	window.addEventListener('scroll', (e) => isInViewport(qBubble) && qBubble.classList.add('active'))
-	mbIcon.addEventListener('click', (e) => document.body.classList.toggle('menu-open'))
+	mbIcon.addEventListener('click', (e) => {
+		e.preventDefault()
+		document.documentElement.classList.toggle('menu-open')
+		document.body.classList.toggle('menu-open')
+	})
 })()
