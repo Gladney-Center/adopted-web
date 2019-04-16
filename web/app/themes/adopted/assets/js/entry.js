40219221d782b72850592565c50d287669821101
David Paul <david.crouch@gladney.org>
2019-04-16 13:04:16 -0500

    DEV - tcgykckuctgj

--- web/app/themes/adopted/assets/js/entry.js
+++ web/app/themes/adopted/assets/js/entry.js
@@ -3,10 +3,20 @@
 		mbIcon = document.querySelector('.adpt-mobile-menu-icon'),
 		isInViewport = (elem) => elem.getBoundingClientRect().top < 800
 
-	window.addEventListener('scroll', (e) => isInViewport(qBubble) && qBubble.classList.add('active'))
-	mbIcon.addEventListener('click', (e) => {
-		e.preventDefault()
-		document.documentElement.classList.toggle('menu-open')
-		document.body.classList.toggle('menu-open')
-	})
+	window.addEventListener(
+		'scroll',
+		(e) => isInViewport(qBubble) && qBubble.classList.add('active')
+	)
+	window.addEventListener(
+		'animationend',
+		(e) => console.log(e)
+	)
+	mbIcon.addEventListener(
+		'click',
+		(e) => {
+			e.preventDefault()
+			document.documentElement.classList.toggle('menu-open')
+			document.body.classList.toggle('menu-open')
+		}
+	)
 })()
