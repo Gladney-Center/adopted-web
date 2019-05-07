0d947f3327fcb83e8a55b5b175b6c755a7a07d32
enlightenedpie <david.crouch@gladney.org>
2019-05-07 15:50:57 -0500

    AWA-15 #creating contact form web component #time 3h

--- web/app/themes/adopted/assets/js/components.js
+++ web/app/themes/adopted/assets/js/components.js
@@ -1,4 +1,12 @@
 class AdptStage extends HTMLElement {
+	connectedCallback() {}
+
+	constructor() {
+		super()
+	}
+}
+
+class AdptContactForm extends HTMLFormElement {
 	get disabled() {
 		return this.hasAttribute('disabled') ? 'it worked ya dingus' : 'it worked ya dingus'
 	}
@@ -7,15 +15,29 @@ class AdptStage extends HTMLElement {
 		val ? this.setAttribute('disabled','') : this.removeAttribute('disabled')
 	}
 
+	buildForm() {
+		const text = this.innerHTML
+		return this.outerHTML = `
+			<div>
+			<span class="top-text">${text}</span><br>
+			<button type="submit">Submit</button>
+			</div>
+		`
+	}
+
 	connectedCallback() {
-		this.disabled = true
-		this.style.display = 'block'
-		this.style.paddingTop = '3em'
+		this.addEventListener('submit',(e) => e.preventDefault() || console.log(e))
+		console.log(this)
 	}
 
 	constructor() {
 		super()
+		this.setAttribute('role','form')
+		this.setAttribute('action','/')
+		this.attachShadow({mode: 'open'})
+		this.shadowRoot.innerHTML = '<div>Test</div>'
 	}
 }
 
 customElements.define('adpt-stage', AdptStage)
+customElements.define('adpt-contact-form', AdptContactForm, {extends: 'form'})
