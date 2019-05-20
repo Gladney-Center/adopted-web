21dfa4bb3dc0760c7e4833477f6e34367a0d9a21
enlightenedpie <david.crouch@gladney.org>
2019-05-20 15:24:44 -0500

    AWA-14 #comment added event listeners for team bio #time 30m

--- web/app/themes/adopted/assets/js/components/team-bio/team-bio.js
+++ web/app/themes/adopted/assets/js/components/team-bio/team-bio.js
@@ -2,6 +2,15 @@ customElements.define('adpt-team-bio',
     class extends HTMLElement {
         connectedCallback() {
             this.setAttribute('role','complementary')
+
+            let figs = this.querySelectorAll('figure')
+            for (var i = 0; i < figs.length; i++) {
+                figs[i].classList.remove('active')
+                figs[i].addEventListener('click',(e) => {
+                    e.preventDefault()
+                    e.target.classList.add('active')
+                })
+            }
         }
 
         constructor() {
