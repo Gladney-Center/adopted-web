fa41960c2ed513e56d09a1f55e0b48471f575949
David Paul <david.crouch@gladney.org>
2019-06-06 16:23:49 -0500

    pushing portal updates

--- src/js/portal/Adpt.js
+++ src/js/portal/Adpt.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import AdptLogo from './components/AdptLogo'
+import AdptNavMenu from './components/AdptNavMenu'
+
+const toggleMenu = () => document.body.classList.toggle('menu-open')
+
+const toggleTheme = (theme) => document.documentElement.className = theme+'-theme' || ''
 
 const Adpt = () =>
     <React.Fragment>
+        <menu onClick={toggleMenu}>
+            <AdptNavMenu toggleTheme={toggleTheme}/>
+        </menu>
         <header>
-            <div className="adpt-mobile-menu">
+            <div className="adpt-mobile-menu" onClick={toggleMenu}>
                 <div className="adpt-mobile-menu-icon">
                     <div className="adpt-mobile-menu-icon-lines"></div>
                 </div>
             </div>
             <AdptLogo />
-            <div className="header-right"></div>
         </header>
         <main></main>
     </React.Fragment>
