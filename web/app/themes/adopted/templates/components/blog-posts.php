93832b779a7b24c5c0651570d3e1d068ea8f20b1
David Paul <david.crouch@gladney.org>
2019-06-07 14:22:36 -0500

    pushing latest changes

--- web/app/themes/adopted/templates/components/blog-posts.php
+++ web/app/themes/adopted/templates/components/blog-posts.php
@@ -9,11 +9,11 @@
 		<div class="adpt-post-blocks">
 		<?php
 			$HAPIkey = HS_HAPI_KEY;
-			$blogcache = get_transient('adpt_blog_datacache');
+			$blogcache = get_transient('adpt_blog_dcache');
 
 			if ($blogcache === false) {
 				$blogcache = wp_remote_get("https://api.hubapi.com/content/api/v2/blog-posts?hapikey=$HAPIkey&limit=3&state=PUBLISHED");
-				set_transient('adpt_blog_datacache',$blogcache,HOUR_IN_SECONDS);
+				set_transient('adpt_blog_dcache',$blogcache,HOUR_IN_SECONDS);
 			}
 			$blog_body = json_decode($blogcache['body'], true);
 
