4de6a536d1d13ec522423266c42d7216182e4dcc
David Paul <enlightenedpie@gmail.com>
2019-08-04 16:47:46 -0500

    AWA-17 #comment finally got Wistia video embeds working #time TooFuckingLong

---
+++ app/components/AdptStudentsVids.js
@@ -0,0 +1,42 @@
+import { Component } from 'react'
+import AdoptEDSan from './AdoptEDSan'
+import PageContext from './context'
+
+export default class AdptStudentsVids extends Component {
+	state = {
+		init: false
+	}
+	
+	componentDidMount() {
+		const wistEmbed = document.createElement('script'),
+			wistExternal = document.createElement('script')
+
+		wistEmbed.src = 'https://fast.wistia.com/embed/medias/qx6z98rzv6.jsonp'
+		wistEmbed.async = true
+		
+		wistExternal.src = 'https://fast.wistia.com/assets/external/E-v1.js'
+		wistExternal.async = true
+
+		document.body.appendChild(wistEmbed)
+		document.body.appendChild(wistExternal)
+
+		this.setState({
+			init: true
+		})
+	}
+
+	render() {
+		if (!this.state.init) return null
+		return (
+			<section className="adpt-videoblock">
+				<adpt-inner>
+					<div className="adpt-video-embed-inner wistia_responsive_padding">
+						<div className="wistia_responsive_wrapper" style={{height: '100%', left: 0, position: 'absolute', top: 0, width: '100%'}}>
+							<span className="wistia_embed wistia_async_qx6z98rzv6 popover=true popoverAnimateThumbnail=true videoFoam=true" style={{display: 'inline-block', height: '100%',position: 'relative', width: '100%'}}>&nbsp;</span>
+						</div>
+					</div>
+				</adpt-inner>
+			</section>
+		)
+	}
+}
--- app/pages/students.js
+++ app/pages/students.js
@@ -3,11 +3,14 @@ import Head from 'next/head'
 import Link from 'next/link'
 import PageContext from '../components/context'
 import SVG from '../components/svg'
+import AdptStudentsVids from '../components/AdptStudentsVids'
 
 const Students = props => {
     const { meta, content } = useContext(PageContext)['students']
 
     document.getElementById('__next').className = 'adpt-students'
+
+    const angieAndJose = {__html: '<script src="https://fast.wistia.com/embed/medias/qx6z98rzv6.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_qx6z98rzv6 popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;position:relative;width:100%">&nbsp;</span></div></div>'}
 
     return (
         <main className="adpt-students">
@@ -23,25 +26,14 @@ const Students = props => {
             <section className="adpt-fold">
                 <adpt-inner>
                     <div className="adpt-fold-graybox">
-                        <p>When you or one of your friends find themselves pregnant (and they sure didn’t mean to be!), we believe that adoption can be a healthy choice because it’s a decision made in the best interest of the child. Because of age, school, and living with parents, you won’t always have what’s needed to provide the things you want to and need to for your child. Especially, if there’s relationship drama thrown into the situation.</p>
-                        <p>We also believe adoption is a good choice for adults who want to be parents but just can’t get pregnant. Adoption can be a great solution for both the expectant parents and the couples who can’t get pregnant.</p>
-                        <p>Learn more about the journey of adoption through Angie and Jose’s story</p>
+                        {content.fold.text.map((el,l) => {
+                            let El = el.el
+                            return <El key={l+90}>{el.text}</El>
+                        })}
                     </div>
                 </adpt-inner>
             </section>
-            <section>
-                <script src="https://fast.wistia.com/embed/medias/qx6z98rzv6.jsonp" async></script>
-                <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
-                <div className="wistia_responsive_padding" style={{padding: '56.25% 0 0 0',position: 'relative'}}>
-                    <div className="wistia_responsive_wrapper" style={{height:'100%', left:0, position: 'absolute', top:0, width:'100%'}}>
-                        <div className="wistia_embed wistia_async_qx6z98rzv6 videoFoam=true" style={{height: '100%', position: 'relative', width: '100%'}}>
-                            <div className="wistia_swatch" style={{height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width:'100%'}}>
-                                <img src="https://fast.wistia.com/embed/medias/qx6z98rzv6/swatch" style={{filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%'}} alt="" aria-hidden="true" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <AdptStudentsVids/>
         </main>
     )
 }
